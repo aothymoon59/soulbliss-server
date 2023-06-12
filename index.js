0d419e51101cedfd8df21dd77358a11873085421
aothymoon59 <mmoon8745@gmail.com>
2023-06-12 16:33:08 +0600

    delete seleced class api done

--- index.js
+++ index.js
@@ -241,6 +241,14 @@ async function run() {
       }
     });
 
+    // delete a selected class
+    app.delete("/selected/delete/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await selectedCollection.deleteOne(filter);
+      res.send(result);
+    });
+
     // get specific user selected class
     app.get("/selected/:email", verifyJWT, async (req, res) => {
       const decodedEmail = req.decoded.email;
